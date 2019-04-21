1eebd20785a26287bbfc9bd62ed6657bd5207861
“liuxincheng <464071996@qq.com>
2019-04-21 12:03:08 +0800

    new master

---
+++ src/main/java/shiro/Single1.java
@@ -0,0 +1,16 @@
+package shiro;
+
+public class Single1 {
+    private static volatile Single1 single1;
+    private Single1(){}
+    public static Single1 getInstance(){
+        if (single1==null){
+            synchronized (Single1.class){
+                if (single1==null){
+                    single1 = new Single1();
+                }
+            }
+        }
+        return single1;
+    }
+}
