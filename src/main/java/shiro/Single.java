3be9809bb408a37c339d9c002080612d870d0f52
“liuxincheng <464071996@qq.com>
2019-04-21 11:35:30 +0800

    new1

--- src/main/java/shiro/Single.java
+++ src/main/java/shiro/Single.java
@@ -1,11 +1,11 @@
 package shiro;
 
 public class Single {
-    private final static Single instance = new Single();
+    private final static Single single = new Single();
 
     private Single(){}
 
     public static Single getInstance() {
-            return instance;
+            return single;
     }
 }
